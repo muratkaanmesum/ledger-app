8cd804166080c923a352cd3974225d6860caa0e8
muratkaanmesum <kaanmesum@gmail.com>
2025-01-09 16:42:02 +0300

    2.3 Implement TransactionService with:     Credit/debit operations     Transfer between accounts     Transaction rollback mechanism     START

--- repositories/transaction_repository.go
+++ repositories/transaction_repository.go
@@ -6,67 +6,55 @@ import (
 	"ptm/models"
 )
 
-type TransactionRepository struct {
-	tx *gorm.DB
-}
+type TransactionRepository struct{}
 
 type ITransactionRepository interface {
-	StartTransaction() error
-	CommitTransaction() error
-	RollbackTransaction() error
-	CreateTransaction(transaction *models.Transaction) error
+	StartTransaction() (*gorm.DB, error)
+	CommitTransaction(tx *gorm.DB) error
+	RollbackTransaction(tx *gorm.DB) error
+	CreateTransaction(tx *gorm.DB, transaction *models.Transaction) error
 	GetTransactionByID(id uint) (*models.Transaction, error)
 	GetAllTransactions() ([]models.Transaction, error)
-	UpdateTransaction(transaction *models.Transaction) error
-	DeleteTransaction(id uint) error
+	UpdateTransaction(tx *gorm.DB, transaction *models.Transaction) error
+	DeleteTransaction(tx *gorm.DB, id uint) error
 }
 
 func NewTransactionRepository() *TransactionRepository {
 	return &TransactionRepository{}
 }
 
-func (r *TransactionRepository) StartTransaction() error {
-	if r.tx != nil {
-		return nil
+func (r *TransactionRepository) StartTransaction() (*gorm.DB, error) {
+	tx := db.DB.Begin()
+	if tx.Error != nil {
+		return nil, tx.Error
 	}
-	r.tx = db.DB.Begin()
-	return r.tx.Error
+	return tx, nil
 }
 
-func (r *TransactionRepository) CommitTransaction() error {
-	if r.tx == nil {
+func (r *TransactionRepository) CommitTransaction(tx *gorm.DB) error {
+	if tx == nil {
 		return nil
 	}
-	err := r.tx.Commit().Error
-	r.tx = nil
-	return err
+	return tx.Commit().Error
 }
 
-func (r *TransactionRepository) RollbackTransaction() error {
-	if r.tx == nil {
+func (r *TransactionRepository) RollbackTransaction(tx *gorm.DB) error {
+	if tx == nil {
 		return nil
 	}
-	err := r.tx.Rollback().Error
-	r.tx = nil
-	return err
+	return tx.Rollback().Error
 }
 
-func (r *TransactionRepository) CreateTransaction(transaction *models.Transaction) error {
-	if r.tx != nil {
-		return r.tx.Create(transaction).Error
+func (r *TransactionRepository) CreateTransaction(tx *gorm.DB, transaction *models.Transaction) error {
+	if tx != nil {
+		return tx.Create(transaction).Error
 	}
 	return db.DB.Create(transaction).Error
 }
 
 func (r *TransactionRepository) GetTransactionByID(id uint) (*models.Transaction, error) {
 	var transaction models.Transaction
-	var err error
-	if r.tx != nil {
-		err = r.tx.First(&transaction, id).Error
-	} else {
-		err = db.DB.First(&transaction, id).Error
-	}
-	if err != nil {
+	if err := db.DB.First(&transaction, id).Error; err != nil {
 		return nil, err
 	}
 	return &transaction, nil
@@ -74,28 +62,22 @@ func (r *TransactionRepository) GetTransactionByID(id uint) (*models.Transaction
 
 func (r *TransactionRepository) GetAllTransactions() ([]models.Transaction, error) {
 	var transactions []models.Transaction
-	var err error
-	if r.tx != nil {
-		err = r.tx.Find(&transactions).Error
-	} else {
-		err = db.DB.Find(&transactions).Error
-	}
-	if err != nil {
+	if err := db.DB.Find(&transactions).Error; err != nil {
 		return nil, err
 	}
 	return transactions, nil
 }
 
-func (r *TransactionRepository) UpdateTransaction(transaction *models.Transaction) error {
-	if r.tx != nil {
-		return r.tx.Save(transaction).Error
+func (r *TransactionRepository) UpdateTransaction(tx *gorm.DB, transaction *models.Transaction) error {
+	if tx != nil {
+		return tx.Save(transaction).Error
 	}
 	return db.DB.Save(transaction).Error
 }
 
-func (r *TransactionRepository) DeleteTransaction(id uint) error {
-	if r.tx != nil {
-		return r.tx.Delete(&models.Transaction{}, id).Error
+func (r *TransactionRepository) DeleteTransaction(tx *gorm.DB, id uint) error {
+	if tx != nil {
+		return tx.Delete(&models.Transaction{}, id).Error
 	}
 	return db.DB.Delete(&models.Transaction{}, id).Error
 }
