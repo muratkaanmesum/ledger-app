ea895dfc887a6d12d72f33986a93facbab086ca4
muratkaanmesum <kaanmesum@gmail.com>
2025-01-13 00:08:53 +0300

    authenticate done and refactored the role based authorization to use user

--- internal/routes/auth_routes.go
+++ internal/routes/auth_routes.go
@@ -1,8 +1,11 @@
 package routes
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
+	"net/http"
 	"ptm/controllers"
+	"ptm/internal/middlewares"
 )
 
 func RegisterAuthRoutes(e *echo.Echo) {
@@ -10,4 +13,8 @@ func RegisterAuthRoutes(e *echo.Echo) {
 
 	route.POST("/register", controllers.RegisterUser)
 	route.POST("/login", controllers.AuthenticateUser)
+	route.POST("/test", func(c echo.Context) error {
+		fmt.Println("entered")
+		return c.String(http.StatusOK, "ok")
+	}, middlewares.JWTAuthenticate, middlewares.RoleBasedAuthorization([]string{"admin", "user"}))
 }
