2ef31aaad48d0913ca5c1643aafab2ef3f24cb90
muratkaanmesum <kaanmesum@gmail.com>
2025-01-28 13:02:29 +0300

    basic rate limiting applied

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,11 +3,13 @@ package routes
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/time/rate"
 	"net/http"
 	"ptm/internal/db/seeder"
 	"ptm/internal/middlewares"
 	v1 "ptm/internal/routes/v1"
 	"ptm/internal/utils/response"
+	"time"
 )
 
 func InitRoutes(e *echo.Echo) {
@@ -18,6 +20,22 @@ func InitRoutes(e *echo.Echo) {
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
 		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}))
+	e.Use(middleware.RateLimiterWithConfig(middleware.RateLimiterConfig{
+		Skipper: middleware.DefaultSkipper,
+		Store: middleware.NewRateLimiterMemoryStoreWithConfig(
+			middleware.RateLimiterMemoryStoreConfig{Rate: rate.Limit(10), Burst: 30, ExpiresIn: 3 * time.Minute},
+		),
+		IdentifierExtractor: func(ctx echo.Context) (string, error) {
+			id := ctx.RealIP()
+			return id, nil
+		},
+		ErrorHandler: func(context echo.Context, err error) error {
+			return context.JSON(http.StatusForbidden, nil)
+		},
+		DenyHandler: func(context echo.Context, identifier string, err error) error {
+			return context.JSON(http.StatusTooManyRequests, nil)
+		},
 	}))
 	e.Use(middlewares.JWTAuthenticate())
 	e.Use(middlewares.ErrorMiddleware())
