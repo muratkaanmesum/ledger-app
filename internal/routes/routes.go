bdfe8fd3342ee0c906e28f9e4395237c6b385a30
muratkaanmesum <kaanmesum@gmail.com>
2025-01-28 13:00:26 +0300

    cors middleware applied

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"net/http"
 	"ptm/internal/db/seeder"
 	"ptm/internal/middlewares"
 	v1 "ptm/internal/routes/v1"
@@ -13,6 +14,11 @@ func InitRoutes(e *echo.Echo) {
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}))
 	e.Use(middlewares.JWTAuthenticate())
 	e.Use(middlewares.ErrorMiddleware())
 	e.Use(middlewares.PerformanceMiddleware())
